66f3e489b9d3fecb919836681197e5116958322d
이찬영 <dlcksdud3579@naver.com>
2018-11-21 23:04:08 +0900

    no message

--- adjList_dfs/Btree/main.c
+++ adjList_dfs/Btree/main.c
@@ -1,5 +1,5 @@
 // 2014105076 이찬영 8주차 실습 B트리 생성 
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -17,12 +17,12 @@
 
 typedef struct node
 {
-	node *child[3];
-	node *parent;
-	int data[2];
-	int n;
-	bool leaf;
-} node;
+	struct node left;
+	struct node right;
+	struct node bro;
+	struct node parent;
+	int data;
+}node;
 
 void Insert(int x); //  인설트하는 함수
 node* createNode(int x, node* parent);
@@ -36,15 +36,16 @@ int main(int argc, char* argv[])
 	int nodeNum;
 	int temp = 0;
 	int unbalanced = 0;
-	freopen(argv[1],"r",stdin);
+	freopen(argv[1],"r", stdin);
 	freopen(argv[2],"w",stdout);
 
-	scanf_s("%d", &nodeNum);
+	scanf("%d", &nodeNum);
 
 	for (int i = 0; i < nodeNum; i++)  // 입력하는 함수 
 	{
-		scanf_s("%d", &temp);
-		avlInsert(&root, temp, &unbalanced);
+		scanf("%d", &temp);
+		Insert(temp);
+
 	}
 
 	fclose(stdin);
@@ -57,81 +58,97 @@ void Insert(int a)
 	int i, temp;
 	node* x = root;
 
-	if (x == NULL)
+	if (x == NULL)  // 루트가 0인경우 
 	{
 		root = createNode(a,NULL);
 		return;
 	}
-	while (x->leaf != true)  // 리프노드 일 경우 서치를 종료
+	while (x->left != NULL)  // 리프노드 일 경우 서치를 종료
 	{
-		
-		if (a < x->data[0])
+		if (a < x->data)
 		{
-			x = x->child[0];
+			x = x->left;
 		}
-		else if (a < x->data[1])
+		else if (a < x->bro->data)
 		{
-			x = x->child[1];
+			x = x->right;
 		}
-		else if(a > x->data[1])
+		else if(a > x->bro->data)
 		{
-			x = x->child[2];
+			x = x->bro->right;
 		}
 		else
 		{
 			printf_s("이미 존재하는 키입니다.");
 		}
 	}
-	if (x->n < 2)  // 리프 노드에 자리가 있을 경우
+	if (x->bro == NULL)  // 리프 형제에 자리가 있을 경우
 	{
-		x->data[1] = a;
-		if (x->data[1] < x->data[0])
-			swap(x->child[0], x->child[1]);
+		x->bro =createNode(a,NULL);
+		if (x->data < x->bro->data)
+			SWAP(x->data, x->bro->data);
 		return;
 	}
-	else  // 리프 노드에 자리가 없을 경우
+	else
 	{
-		int insetKey = a;
+		node *tempNode = createNode(a, NULL);
+		node *nextParent = NULL;
 		while (x->parent != NULL)
 		{
-			x->n = 1;
-			if (insetKey < x->data[0])
+			if (tempNode->data < x->data)
 			{
-				swap(x->data[0], insetKey);
-				x = x->parent;
+				SWAP(x->data, tempNode->data);
+				tempNode->right = x;
+				tempNode->right = x->bro;
+				nextParent = x->parent;
+				x->parent = tempNode->parent;
 			}
-			else if (insetKey < x->data[1])
+			else if (tempNode->data < x->bro->data)
 			{
-				x = x->parent;
+				
 			}
-			else if (insetKey > x->data[1])
+			else if (tempNode->data > x->bro->data)
 			{
-				swap(x->data[1], insetKey);
-				x = x->parent;
+				SWAP(x->bro->data, tempNode->data);
+				
 			}
 			else
 			{
 				printf_s("이미 존재하는 키입니다.");
+				return;
 			}
+			if (x->n < 2)
+			{
+				x->data[1] = insetKey;
+				if (x->data[1] < x->data[0])
+					SWAP(x->child[0], x->child[1]);
+				x->n = 2;
+				return;
+			}
+			x = x->parent;
 		}
 
 
+		node* newRoot = createNode(insetKey,NULL);
+		node* rootrightChild = createNode(root->data[1], newRoot);
+
+		root->data[1] = 0;
+		root->n = 0;
+		root->parent = newRoot;
+
+
+		
 
 	}
-
-
 }
+
 node* createNode(int x,node* parent)
 {
 	node* temp = (node*)malloc(sizeof(node));
-	temp->n = 1;
-	temp->child[0] = NULL;
-	temp->child[1] = NULL;
-	temp->child[2] = NULL;
-
-	temp->data[0] = x;
-	temp->data[1] = 0;
-	temp->leaf = true;
+	temp->left = NULL;
+	temp->right = NULL;
+	temp->bro = NULL;
+	temp->data = x;
 	temp->parent = parent;
 	return temp;
 }
